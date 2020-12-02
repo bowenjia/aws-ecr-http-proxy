2ee45448442a54dcbe77257863a82c2b65d26312
Bowen Jia <bowenjia@gmail.com>
2020-12-02 13:27:19 +1300

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+IMAGE_REPO="nginx"  
+IMAGE_VERSION=(1.1 latest)
+
+for index in "${IMAGE_VERSION[@]}";
+  do
+    echo $IMAGE_REPO
+    docker tag nginx $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$index
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$index
+    echo $index;
+  done    
+        
+docker tag nginx:latest 515539806848.dkr.ecr.ap-southeast-2.amazonaws.com/nginx:latest
